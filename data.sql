3ef55720b7ff83577a1a5a473574e1d8325e83c1
yurat <ytatisch@gmail.com>
2015-08-27 02:09:54 +0000

    Months in database

--- data.sql
+++ data.sql
@@ -1,4 +1,3 @@
-DROP TABLE months;
 
 CREATE TABLE months (id INTEGER PRIMARY KEY AUTO_INCREMENT, month TEXT, number_of_days INTEGER, season TEXT);
 
@@ -13,4 +12,4 @@ INSERT INTO months (month, number_of_days, season) VALUES ("August", 31, "Summer
 INSERT INTO months (month, number_of_days, season) VALUES ("September", 30, "Fall");
 INSERT INTO months (month, number_of_days, season) VALUES ("October", 31, "Fall");
 INSERT INTO months (month, number_of_days, season) VALUES ("November", 30, "Fall");
-INSERT INTO months (month, number_of_days, season) VALUES ("December", 31, "Winter");
+INSERT INTO months (month, number_of_days, season) VALUES ("ааа", 31, "Winter");
