4616fc918403845903acf7ea9bf43333c1978472
yurat <ytatisch@gmail.com>
2015-10-09 04:14:09 +0000

    completed change password function

--- change_password.php
+++ change_password.php
@@ -1,6 +1,8 @@
 <?php
     $hash_password = "";
-    $hash_password = hash("sha256", $_POST["password"]);
+    $hash_password = hash("sha256", $_POST["old_password"]);
+    $hash_new = "";
+    $hash_new = hash("sha256", $_POST["new_password"]);
 
     if ($_COOKIE["user_id"] === null) {
         echo "Please login again";
@@ -12,7 +14,7 @@
         echo "You can't have null password";
     }
     else if ($_POST["old_password"] == $_POST["new_password"]) {
-        header("Location: notes.php");
+        echo "You already have the same password";
     }
     else {
         include_once("function.php");
@@ -31,7 +33,19 @@
         if ($hash_password != $hash_check) {
             echo "old password does not match";
         }
-        
-        
+        else {
+            $query = "UPDATE users SET password = '" . $hash_new . "' WHERE ID = " . $user_id;
+            $statment = $pdo->prepare($query);
+            
+            if($statment->execute()) {
+                echo "Password successfully changed";
+            }
+        }
     }
 ?>
+
+<br />
+<br />
+
+<button><a href="notes.php" style="color: blue;
+text-decoration: none;">Go Back</a></button>
